0c04b9ae624f54e083c5877fb242187764933dae
Lior Agnin <lior.agnin@gmail.com>
2020-10-19 13:52:20 +0300

    f

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const WebappWebpackPlugin = require('webapp-webpack-plugin')
 const config = require('config')
-
+ 
 const isDev = process.env.NODE_ENV === 'development'
 const sourceMap = isDev
 

4ec809aef341ff507d05dbd70b4d87810f77a712
Lior Agnin <lior.agnin@gmail.com>
2020-11-21 14:08:48 +0200

    fixed AddLiquidity link

--- src/components/common/AddLiquidity.jsx
+++ src/components/common/AddLiquidity.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import replace from 'lodash/replace'
 import { useSpring, animated } from 'react-spring'
 import alertIcon from '@/assets/images/alert.svg'
 
@@ -8,6 +10,8 @@ const AddLiquidity = () => {
     transform: 'translate(0px, 0px)',
     from: { opacity: 0, transform: 'translate(-20px, -20px)' }
   })
+  const { lpToken, pairName, networkId } = useSelector(state => state.staking)
+  const symbol = replace(pairName, '/', '-')
 
   return (
     <animated.div style={props}>
@@ -18,16 +22,23 @@ const AddLiquidity = () => {
               <img src={alertIcon} />
               <div className='text'>
                 <div className='beta'>Beta - use at your own risk.</div>
-                Please add liquidity to the Uniswap FUSE-ETH pool and deposit the LP token received here to mine more FUSE.
+                Please add liquidity to the {networkId === 1 ? 'Uniswap' : 'FuseSwap'} {symbol} pool and deposit the LP token received here to mine more FUSE.
                 <br />
-                Please refer to the guide for more details.
+                <a
+                  rel='noreferrer noopener'
+                  target='_blank'
+                  href='https://medium.com/fusenet/how-to-stake-eth-fuse-lp-tokens-for-fuse-rewards-fd9abe08f84c'
+                >
+                  Please refer to the guide for more details.
+                </a>
               </div>
             </div>
+            {/* TODO - add FuseSwap pair link after release */}
             <a
               rel='noreferrer noopener'
               target='_blank'
               className='cell medium-6 small-24'
-              href='https://app.uniswap.org/#/add/0x970B9bB2C0444F5E81e9d0eFb84C8ccdcdcAf84d/ETH'
+              href={`https://app.uniswap.org/#/add/${lpToken}/ETH`}
             >
               <button className='button'>
                 Add liquidity on Uniswap

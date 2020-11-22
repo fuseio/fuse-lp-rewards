0cce84ccd8fc517265ca81a350f062586bc1029f
Lior Agnin <lior.agnin@gmail.com>
2020-11-22 12:04:53 +0200

    oops

--- src/components/common/AddLiquidity.jsx
+++ src/components/common/AddLiquidity.jsx
@@ -6,9 +6,9 @@ import alertIcon from '@/assets/images/alert.svg'
 
 const getSwapLink = (pairs, networkId) => {
   if (networkId === 1) {
-    return `https://app.uniswap.org/#/add/${pairs.toString().split(',').join('/')}`
+    return `https://app.uniswap.org/#/add/${pairs?.toString()?.split(',')?.join('/')}`
   } else if (networkId === 122) {
-    return `https://beta.fuseswap.com/#/add/${pairs.toString().split(',').join('/')}`
+    return `https://beta.fuseswap.com/#/add/${pairs?.toString()?.split(',')?.join('/')}`
   }
 }
 

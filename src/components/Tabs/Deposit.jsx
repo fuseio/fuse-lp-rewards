92e1aa1d1e69abebe02190cd2631e43d69b0c35c
Lior Agnin <lior.agnin@gmail.com>
2020-11-24 20:16:31 +0200

    added enableReinitialize

--- src/components/Tabs/Deposit.jsx
+++ src/components/Tabs/Deposit.jsx
@@ -137,6 +137,7 @@ const DepositForm = ({ handleConnect }) => {
       validationSchema={Scheme}
       render={renderForm}
       onSubmit={onSubmit}
+      enableReinitialize
       validateOnChange
     />
   )

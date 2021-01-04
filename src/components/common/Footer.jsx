476b9a4ad9e1aaba1f6360aa002c69451386f312
Lior Agnin <lior.agnin@gmail.com>
2021-01-04 11:08:47 +0200

    update to 2021

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -87,7 +87,7 @@ const Footer = () => {
         </div>
       </div>
       <div className='footer__text cell medium-8 grid-x align-start'>
-        © 2020 Fuse. The Fuse network is a permissionless and border-less public ledger designed for easy integration of everyday payments.
+        © 2021 Fuse. The Fuse network is a permissionless and border-less public ledger designed for easy integration of everyday payments.
       </div>
     </footer>
   )

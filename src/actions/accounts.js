62bf5399c3516ed3fcd049ad0e7ec7f3840e9f6b
Leon Prouger <leonprou@gmail.com>
2021-08-09 17:58:55 +0300

    Passing blocknumber for fetch calls to support WC (#66)

--- src/actions/accounts.js
+++ src/actions/accounts.js
@@ -3,5 +3,5 @@ import { createRequestTypes, action } from './utils'
 export const BALANCE_OF_TOKEN = createRequestTypes('BALANCE_OF_TOKEN')
 export const BALANCE_OF_NATIVE = createRequestTypes('BALANCE_OF_NATIVE')
 
-export const balanceOfToken = (tokenAddress) => action(BALANCE_OF_TOKEN.REQUEST, { tokenAddress })
+export const balanceOfToken = (tokenAddress, blockNumber) => action(BALANCE_OF_TOKEN.REQUEST, { tokenAddress, blockNumber })
 export const balanceOfNative = (accountAddress) => action(BALANCE_OF_NATIVE.REQUEST, { accountAddress })
